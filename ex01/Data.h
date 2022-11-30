c6b833228d5e4bc419b77c264c321a8da2a7725f
Lucas Sinke <lsinke@student.codam.nl>
2022-11-30 22:36:50 +0100

    Add ex01

---
+++ ex01/Data.h
@@ -0,0 +1,12 @@
+#ifndef DATA_H
+# define DATA_H
+
+struct Data {
+	int a;
+	int b;
+	int c;
+	int d;
+	char efghijk[8];
+};
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <iostream>
+#include <cstring>
+#include "Data.h"
+
+uintptr_t serialize(Data *ptr) {
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data *deserialize(uintptr_t raw) {
+	return reinterpret_cast<Data *>(raw);
+}
+
+int main() {
+	Data data = {'a', 'b', 'c', 'd', "efghijk"};
+	uintptr_t raw = serialize(&data);
+	Data *ptr = deserialize(raw);
+	if (&data != ptr)
+		std::cerr << "Pointer is different! " << ptr << " vs " << &data << "\n";
+	if (std::memcmp(&data, ptr, sizeof(Data)) != 0)
+		std::cerr << "Data is different!\n";
+}
